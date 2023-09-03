bad4a97aa4d80dec6edf3b4c59a6c06cc99e53ce
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-09-03 19:12:25 -0300

    Realizando o componente rodapé

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -5,28 +5,28 @@ import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/
 
 export default function Rodape() {
 
-  return (
-    <footer className="footer">
-      <div className="footer-content">
-        <div className="social-media">
-          <a href="#" className="social-link">
-            <FontAwesomeIcon icon={faFacebookF} />
-          </a>
-          <a href="#" className="social-link">
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-          <a href="#" className="social-link">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-          <a href="#" className="social-link">
-            <FontAwesomeIcon icon={faLinkedinIn} />
-          </a>
-        </div>
-        <div className="footer-info">
-          <p>&copy; 2023 iBella Digital Devices. </p>
-          <p>Todos os direitos reservados.</p>
-        </div>
-      </div>
-    </footer>
-  )
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="social-media">
+                    <a href="#" className="social-link">
+                        <FontAwesomeIcon icon={faFacebookF} />
+                    </a>
+                    <a href="#" className="social-link">
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                    <a href="#" className="social-link">
+                        <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                    <a href="#" className="social-link">
+                        <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
+                </div>
+                <div className="footer-info">
+                    <p>&copy; 2023 iBella Digital Devices. </p>
+                    <p>Todos os direitos reservados.</p>
+                </div>
+            </div>
+        </footer>
+    )
 }
