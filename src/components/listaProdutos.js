3af51df622d2ea7382686516b24dba940d7db409
IsabellajFerreira <isabellajferreira@hotmail.com>
2023-09-04 21:53:09 -0300

    Montando a lista de produts em js, que vão ser chamadas na tela

--- src/components/listaProdutos.js
+++ src/components/listaProdutos.js
@@ -1,17 +1,32 @@
 export const listaProdutos = [
     {
         id: 1,
-        nome: 'Teclado',
-        preco: 150
+        nome: 'Iphone 11',
+        preco: 2650,
+        cor: 'preto'
     },
     {
         id: 2,
-        nome: 'Mouse',
-        preco: 120
+        nome: 'Samsung Galaxy S23 Ultra 5G',
+        preco: 9000,
+        cor: 'branco'
     },
     {
         id: 3,
-        nome: 'Monitor',
-        preco: 950
+        nome: 'iPad Apple 10th',
+        preco: 3400,
+        cor: 'azul'
+    },
+    {
+        id: 4,
+        nome: 'Kindle 11ª Geração',
+        preco: 474,
+        cor: 'preto'
+    },
+    {
+        id: 5,
+        nome: 'Galaxy Tab S9',
+        preco: 10.349,
+        cor: 'grafite'
     },
 ]
